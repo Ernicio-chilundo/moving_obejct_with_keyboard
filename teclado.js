0fd785ecf013b15a318eb5387beb01beff0b2134
Ern-2022 <erniciojermiaschilund@gmail.com>
2024-07-10 16:01:21 +0200

    add

--- teclado.js
+++ teclado.js
@@ -23,3 +23,28 @@ Teclado.prototype ={
         return this.pressionadas[tecla]
     }
 }
+
+// var SETA_ESQUERDA = 37
+// var SETA_DIREITA = 39
+
+// function Teclado(elemento){
+//     this.elemento = elemento
+
+//     this.pressionadas = []
+
+//     var teclado = this
+
+//     elemento.addEventListener("heydown", function(evento){
+//         teclado.pressionadas[evento.keyCode]=true
+//     })
+
+//     elemento.addEventListener("keyup", function(evento){
+//         teclado.pressionadas[evento.keyCode]=false
+//     })
+// }
+
+// Teclado.prototype = {
+//     pressioda:function(tecla){
+//         return this.pressionadas[tecla]
+//     }
+// }
